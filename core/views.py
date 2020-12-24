61c82047a1262ccd8f490ed2753d4d2c93f899ac
Gracious <graciousnguwo56@gmail.com>
2020-12-24 13:21:15 +0200

    first commit

---
+++ core/views.py
@@ -0,0 +1,29 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# third party imports
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import PostSerializer
+from .models import Post
+
+
+class TestView(APIView):
+    permission_classes =(IsAuthenticated, )
+    
+    def get(self, request, *args, **kwargs):
+        qs = Post.objects.all()
+        post = qs.first ()
+        serializer=PostSerializer(post)
+        
+        return Response(serializer.data)
+    
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors) 
